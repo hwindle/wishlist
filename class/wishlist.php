401235b3054feb89e45670d4efeeeac2e0138989
Hazel Windle <hwindle83@googlemail.com>
2022-05-20 17:39:34 +0100

    Created wishlist class with updateItem, deleteItem methods

--- class/wishlist.php
+++ class/wishlist.php
@@ -93,6 +93,55 @@
     }
 
     // update 1 item
+    public function updateItem() {
+      $sqlQuery = 'UPDATE ' . $this->dbTable .
+          ' SET 
+           item = :item, category = :category,
+                shop_name = :shop_name, url = :url,
+                description = :description, quantity = :quantity,
+                price = :price, room = :room, priority = :priority 
+           WHERE wishlist_id = :id';
 
+      $stmt = $this->conn->prepare($sqlQuery);
+      // clean data
+      $this->item = htmlspecialchars(strip_tags($this->item));
+      $this->category = htmlspecialchars(strip_tags($this->category));
+      $this->shop_name = htmlspecialchars(strip_tags($this->shopName));
+      $this->url = htmlspecialchars(strip_tags($this->url));
+      $this->description = htmlspecialchars(strip_tags($this->description));
+      $this->quantity = (int) $this->quantity;
+      $this->price = floatval($this->price);
+      $this->room = htmlspecialchars(strip_tags($this->room));
+      $this->priority = (int) $this->priority;
+      $this->wishlistId = (int) $this->wishlistId;
+      // bind data
+      $stmt->bindParam(':item', $this->item);
+      $stmt->bindParam(':category', $this->category);
+      $stmt->bindParam(':shop_name', $this->shopName);
+      $stmt->bindParam(':url', $this->url);
+      $stmt->bindParam(':description', $this->description);
+      $stmt->bindParam(':quantity', $this->quantity);
+      $stmt->bindParam(':price', $this->price);
+      $stmt->bindParam(':room', $this->room);
+      $stmt->bindParam(':priority', $this->priority);
+      $stmt->bindParam(':id', $this->wishlistId);
+
+      if ($stmt->execute()) {
+        return true;
+      } 
+      return false;
+    }
+
+    // delete 1 item
+    function deleteItem() {
+      $sqlQuery = 'DELETE FROM ' . $this->dbTable . ' WHERE id = ?';
+      $stmt = $this->conn->prepare($sqlQuery);
+      $this->wishlistId = (int) $this->wishlistId;
+      $stmt->bindParam(1, $this->wishlistId);
+      if ($stmt->execute()) {
+        return true;
+      }
+      return false;
+    }
 
   }
