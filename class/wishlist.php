15e2ff2ee3e65d4d36bc8e97139458e9b4deb14e
Hazel Windle <hwindle83@googlemail.com>
2022-05-20 17:32:03 +0100

    Added get all items, get single item to wishlist class

--- class/wishlist.php
+++ class/wishlist.php
@@ -24,8 +24,75 @@
     // fetch all rows
     public function getWishlist() {
       $sqlQuery = 'SELECT * FROM ' . $this->dbTable . '';
-      $stmt = pg_query_params($this->conn, $sqlQuery, []);
+      $stmt = $this->conn->prepare($sqlQuery);
+      $stmt->execute();
       return $stmt; 
     }
+
+    // create one wishlist item
+    public function createWishlist() {
+      $sqlQuery = 'INSERT INTO' . $this->dbTable . 
+            '(item, category, shop_name, url,
+               description, quantity, price, room, priority)
+                VALUES (
+                item = :item, category = :category,
+                shop_name = :shop_name, url = :url,
+                description = :description, quantity = :quantity,
+                price = :price, room = :room, priority = :priority    
+            )';
+      $stmt = $this->conn->prepare($sqlQuery);
+      // clean data
+      $this->item = htmlspecialchars(strip_tags($this->item));
+      $this->category = htmlspecialchars(strip_tags($this->category));
+      $this->shop_name = htmlspecialchars(strip_tags($this->shopName));
+      $this->url = htmlspecialchars(strip_tags($this->url));
+      $this->description = htmlspecialchars(strip_tags($this->description));
+      $this->quantity = (int) $this->quantity;
+      $this->price = floatval($this->price);
+      $this->room = htmlspecialchars(strip_tags($this->room));
+      $this->priority = (int) $this->priority;
+      // bind data
+      $stmt->bindParam(':item', $this->item);
+      $stmt->bindParam(':category', $this->category);
+      $stmt->bindParam(':shop_name', $this->shopName);
+      $stmt->bindParam(':url', $this->url);
+      $stmt->bindParam(':description', $this->description);
+      $stmt->bindParam(':quantity', $this->quantity);
+      $stmt->bindParam(':price', $this->price);
+      $stmt->bindParam(':room', $this->room);
+      $stmt->bindParam(':priority', $this->priority);
+
+      if ($stmt->execute()) {
+        return true;
+      } 
+      return false;
+    }
+
+    // read one item
+    public function getSingleItem() {
+      $sqlQuery = 'SELECT * FROM' . $this->dbTable 
+              . ' WHERE id = ? LIMIT 1';
+      $stmt = $this->conn->prepare($sqlQuery);
+      $stmt->bindParam(1, $this->wishlistId);
+      try {
+        $stmt->execute();
+        $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
+        $this->item = $dataRow['item'];
+        $this->category = $dataRow['category'];
+        $this->shopName = $dataRow['shop_name'];
+        $this->url = $dataRow['url'];
+        $this->description = $dataRow['description'];
+        $this->quantity = $dataRow['quantity'];
+        $this->price = $dataRow['price'];
+        $this->totalPrice = $dataRow['total_price'];
+        $this->room = $dataRow['room'];
+        $this->priority = $dataRow['priority'];
+      } catch (PDOException $e) {
+        echo 'Fetching one item failed: ' . $e->getMessage();
+      }
+    }
+
+    // update 1 item
+
 
   }
