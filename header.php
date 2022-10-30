c1654af446f98c3af6544b730349926a7d90f053
Hazel Windle <hwindle83@googlemail.com>
2022-10-30 14:38:42 +0000

    Finished the navbar, added require_once lines to the index.php file

--- header.php
+++ header.php
@@ -5,15 +5,46 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/bootstrap_spacelab_theme.css">
+  <link rel="stylesheet" href="css/styles.css">
   <title>Home | Wishlist Program</title>
 </head>
 <body>
   <header>
     <h1>Wishlist</h1>
   </header>
-  <nav class="">
+  <nav class="navbar-nav">
     <ul>
-      <li>Home</li>
+      <li class="nav-item">
+        <a href="index.php" class="nav-link"><span id="home"></span>
+          Home
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="add_current.php" class="nav-link"><span class="pencil"></span>
+          Add Current
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="view_wishlist.php" class="nav-link">
+          View Wishlist
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="add_wishlist.php" class="nav-link"><span class="pencil"></span>
+          Add Wishlist
+        </a>
+      </li>
+      <!-- php if/else here -->
+      <li class="nav-item">
+        <a href="login.php" class="nav-link"><span class="pencil"></span>
+          Login
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="logout.php" class="nav-link"><span class="pencil"></span>
+          Logout
+        </a>
+      </li>
     </ul>
   </nav>
   
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+  require_once('header.php');
+
+?>
+
+<!-- list of current items, sort by status -->
+
+<?php
+  require_once('footer.php');
+?>
