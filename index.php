b897341e3878cebece26203c899a980f0298df4d
Hazel Windle <hwindle83@googlemail.com>
2022-11-07 15:01:03 +0000

    Added the HTML for the index current items table view

--- index.php
+++ index.php
@@ -6,7 +6,33 @@
   <a href="register.php">Register</a>
 </button>
 <!-- list of current items, sort by status -->
-
 <?php
+  if ($_SESSION['user_id'] != null):
+?>
+  <h3>Current Items</h3>
+  <table class="table" id="current-items">
+    <thead>
+      <tr>
+        <th>Item</th>
+        <th>Description</th>
+        <th>Status</th>
+        <th>Place</th>
+      </tr>
+    </thead>
+    <tbody>
+      
+    </tbody>
+    <tfoot>
+      <tr>
+        <th>Item</th>
+        <th>Description</th>
+        <th>Status</th>
+        <th>Place</th>
+      </tr>
+    </tfoot>
+  </table>
+  <p class="errors"><?= $e; ?></p>
+<?php
+  endif; // session user logged in check
   require_once('footer.php');
 ?>
