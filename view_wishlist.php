4028077c531dc2a23ec82441d15dfa7a1e4183a6
Hazel Windle <hwindle83@googlemail.com>
2022-12-12 20:12:46 +0000

    Added basic wishlist card CSS in layout

--- view_wishlist.php
+++ view_wishlist.php
@@ -19,13 +19,13 @@
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         foreach ($results as $result) {
           echo '<div class="wishlist-card">';
-          echo "\n<h4>${result['item']}</h4>\n";
+          echo "\n<figure>\n";
+          echo "<img src='./${result['pic']}' class='card-img' alt='${result['item']}'>\n";
+          echo "\n<figcaption>${result['item']}</figcaption>\n";
+          echo "</figure>\n";
           echo "\n<h5>${result['shop_name']}</h5>\n";
           echo '<a class="buy-button" href="' . $result['url'] . '">
             Buy this</a><hr>';
-          echo "\n<figure>\n";
-          echo "<img src='./${result['pic']}' class='card-img' alt='${result['item']}'>\n";
-          echo "</figure>\n";
           echo "\n<p class='prices'>£${result['price']} each, £${result['total_price']}  total for ${result['quantity']}.</p>\n";
           echo "\n<p class='priority'>Priority (100 highest): ${result['priority']}</p>\n";
           echo "\n<p class='category'>Category: ${result['category']}</p>\n";
